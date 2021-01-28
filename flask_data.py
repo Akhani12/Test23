f33bfb99150ca9e8e52a62a82fc1d7e410c839c8
Thakkar Harsh <46391521+Akhani12@users.noreply.github.com>
2021-01-28 21:32:04 +0530

    Add files via upload

---
+++ flask_data.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    # name = request.args['name']
+    # ids = request.args['ids']
+    # return "<h1>My Name Is "+name+" And id is "+str(ids)+"</h1>"
+    return  "hii"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
