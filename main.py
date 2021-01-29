21e5ab503f409f4f0a1ebaa8d891f551dce69a61
Thakkar Harsh <46391521+Akhani12@users.noreply.github.com>
2021-01-29 13:37:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    # name = request.args['name']
-    # ids = request.args['ids']
-    # return "<h1>My Name Is "+name+" And id is "+str(ids)+"</h1>"
+    name = request.args['name']
+    ids = request.args['ids']
+    return "<h1>My Name Is "+name+" And id is "+str(ids)+"</h1>"
     return  "hii"
 
 
