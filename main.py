95972cd7b01de80c93699dafe30c9fcfed587794
Thakkar Harsh <46391521+Akhani12@users.noreply.github.com>
2021-01-30 11:06:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    with open("j.txt", 'w') as f:
-      f.write("harsh")
+    def writeToFile(path, content):
+       file = open(path, "w")
+       file.write(content)
+       file.close()
+  
+  
+    PATH_TO_MY_FILE = './example.txt'
+    CONTENT_FOR_MY_FILE = 'Example\nThis is on line 2 of a text file.\n\nThe end.'
+
+    writeToFile(PATH_TO_MY_FILE, CONTENT_FOR_MY_FILE)
     return  "hii"
 
 
