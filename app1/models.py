d0c1c01f64b4eabc1ebe74e8e99c0428e0bf3ba9
Thakkar Harsh <46391521+Akhani12@users.noreply.github.com>
2022-01-10 11:29:06 +0530

    changes.

--- app1/models.py
+++ app1/models.py
@@ -2,16 +2,23 @@ from django.contrib.auth.models import UserManager
 from django.db import models
 
 
+class Base(models.Model):
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
 class CustomUserManager(UserManager):
     def get_query_set(self):
         return super(CustomUserManager, self).get_query_set().filter(is_delete=False)
 
 
 # Create your models here.
-class Video(models.Model):
+class Video(Base):
     created_at = models.DateTimeField(auto_now_add=True)
     desc = models.CharField(max_length=500)
     image = models.ImageField(upload_to="static/uploadedphoto")
-    links = models.CharField(max_length=10000)
+    code = models.CharField(max_length=100,null=True)
 
     objects = CustomUserManager
--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 from app1.models import Video
 
 
-def Home(request):
-    list_data = list(Video.objects.values('desc', 'image', 'links'))
-    print(list_data)
-    context = {"video_data":list_data}
-    return render(request, "portfolio.html", context=context)
+class Home(TemplateView):
+    def get(self, request, **kwargs):
+        list_data = list(Video.objects.values('desc', 'image','code'))
+        print(list_data)
+        context = {"video_data": list_data}
+        return render(request, "portfolio.html", context=context)
+
+    def post(self, request, **kwargs):
+        email = request.POST.get("email")
+        code = request.POST.get("code")
+        messages.info(request,"Successfully subscribe it.")
+
+        print(email)
+        return redirect("/home/")
+
